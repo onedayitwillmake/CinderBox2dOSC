af85a0614fb0ad29a8e8c4ba0bf36379ae46329b
onedayitwillmake <mariogonzalez@gmail.com>
2011-05-22 00:20:00 -0400

    Initial build, box2d works

---
+++ src/b2cinder/PhysicsElement.cpp
@@ -0,0 +1,23 @@
+/*
+*  DynamicBody.cpp
+*  BasicBox2D
+*
+*  Created by David Wicks on 6/9/10.
+*  Copyright 2010 David Wicks. All rights reserved.
+*
+*/
+
+#include "PhysicsElement.h"
+
+namespace cinder
+{
+	namespace box2d 
+	{
+
+		PhysicsElement::PhysicsElement()
+		{
+			mBody = NULL;
+		}
+
+	}
+}
